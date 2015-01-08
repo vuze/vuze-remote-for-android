540ebcae9a0898274c1d16d3a4b4c6b637288e7b
Arron Mogge <tux@vuze.com>
2015-01-08 14:33:10 -0800

    v2.2.4, Build 63, missed projects (due to .git folders in subdirs)

---
+++ android-switch-backport/sample/gen/org/jraf/android/backport/switchwidget/sample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.jraf.android.backport.switchwidget.sample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
